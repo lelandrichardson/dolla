dd7924f4d524e86f77b90b7c7a04099c6d697567
Leland Richardson <leland.m.richardson@gmail.com>
2013-04-18 22:47:35 -0700

    modified .html()
    - added some utility methods
    - improved compose() to include passed in arguments
    - only assigned dolla to $ if wasn't already assigned
    - added getter for .html() method
    - added string parameter version for .html()
    - made .html() use this.each

--- dolla.js
+++ dolla.js
@@ -1,44 +1,61 @@
 // custom selector implementation
 // nickname: dolla
-;(function() {
+;(function(undefined) {
     'use strict';
 
-    // utility methods
+    // useful native methods
+    // ----------------------------------------------------
+    var slice = Array.prototype.slice;
+
+    // utility functions
     // ----------------------------------------------------
     var nop = function() {};
     var compose = function(a,b){
         return function(){
-            a.call(this);
-            b.call(this);
+            var args = slice.call(arguments,0);
+            a.apply(this, args);
+            b.apply(this, args);
         }.bind(this);
     };
+    // returns true if passed in object is a DOM element...
+    var isElement = function(o) {
+        return (
+            typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+                o && typeof o === "object" && o.nodeType === 1 && typeof o.nodeName==="string"
+            );
+    };
+    var isString = function(s){ return typeof s === 'string';},
+        isFunction = function(f){ return typeof f === 'function';},
+        isArray = Array.isArray, //use native version here
+        isDolla = function(d) {return d instanceof dolla;}
 
     // allows registration of multiple handlers without clobbering existing ones
     var registerEvent = function(el,event,callback){
         el['on' + event] = compose.call(el, el['on' + event] || nop, callback );
     };
 
-    // dolla --------------------------------------------------
+    // PRIMARY CONSTRUCTOR
     var dolla = function(selector, context) {
 
-        var dolla = {}, nodes;
+        var self = {}, nodes;
 
-        if (typeof context === 'string') {
+        if (isString(context)) {
             context = document.querySelector(context);
         }
 
-        nodes = Array.prototype.slice.call((context || document).querySelectorAll(selector));
+        nodes = slice.call((context || document).querySelectorAll(selector));
 
         // add the results to the items object
-        for (var i = 0; i < length; i++) {
-            dolla[i] = nodes[i];
+        for (var i = 0; i < nodes.length; i++) {
+            self[i] = nodes[i];
         }
 
-        dolla.length = nodes.length;
-        dolla.slice = [].slice();
+        self.length = nodes.length;
     };
 
     dolla.fn = dolla.prototype;
+
+    dolla.fn.slice = slice;
 
     // Prototype Methods
     // -----------------------------------------
@@ -49,12 +66,36 @@
         return this;
     };
 
-    dolla.fn.html = function (callback) {
+
+
+    // html function
+    // GETTER: `.html()` -> returns string representation of the node's inner HTML
+    // SETTER:
+    //      `.html(String)` -> set's .innerHTML to String
+    //      `.html(function(index,currentHtml))` -> pass in an iterator function which returns the text you want to use
+    //                                              for the html of the given node.  inside the callback, `this` will
+    //                                              will be bound to the element.
+    dolla.fn.html = function (stringOrCallback) {
+        // if no parameter passed in, simply return innerHTML of first node.
+        if(stringOrCallback === undefined){
+            return this.length > 0 ? this[0].innerHTML : null;
+        }
+        // otherwise, we are *setting* the html
         var response;
-        for (var i = 0; i < this.length; i++) {
-            if (response = callback.call(this[i], i, this[i].innerHTML)) {
-                this[i].innerHTML = response;
-            }
+        if(isString(stringOrCallback)){
+            // setting as a simple sting
+            this.each(function(){
+                this.innerHTML = stringOrCallback;
+            });
+        } else {
+            // using callback function
+            this.each(function(i,el){
+                // user must return explicit `false` in order to skip updating value
+                if ((response = stringOrCallback.call(this, i, this.innerHTML)) !== false) {
+                    this.innerHTML = response;
+                }
+            });
+
         }
         return this;
     };
